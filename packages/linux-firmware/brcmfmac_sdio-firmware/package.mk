9e98310d7a32432bce919c7c80e789b30d20d30d
Christian Hewitt <christianshewitt@gmail.com>
2018-09-26 09:50:12 +0400

    firmware: adapt brcmfmac_sdio-firmware package

---
+++ packages/linux-firmware/brcmfmac_sdio-firmware/package.mk
@@ -0,0 +1,44 @@
+# SPDX-License-Identifier: GPL-2.0-or-later
+# Copyright (C) 2009-2016 Stephan Raue (stephan@openelec.tv)
+# Copyright (C) 2016-present Team LibreELEC (https://libreelec.tv)
+
+PKG_NAME="brcmfmac_sdio-firmware"
+PKG_VERSION="583d9ea01df194b1359ffde970ab91fd71ad9ea8"
+PKG_SHA256="10640e843afff098619d4842109c5cb198c6251e3efed233fc7d006668ea4ba4"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="https://github.com/chewitt/brcmfmac_sdio-firmware"
+PKG_URL="https://github.com/chewitt/$PKG_NAME/archive/$PKG_VERSION.tar.gz"
+PKG_SECTION="firmware"
+PKG_LONGDESC="Broadcom SDIO firmware used with LibreELEC mainline kernels"
+PKG_TOOLCHAIN="manual"
+
+post_makeinstall_target() {
+  FW_TARGET_DIR=$INSTALL/$(get_full_firmware_dir)
+
+  if find_file_path firmwares/$PKG_NAME.dat; then
+    FW_LISTS="${FOUND_PATH}"
+  else
+    FW_LISTS="${PKG_DIR}/firmwares/any.dat ${PKG_DIR}/firmwares/${TARGET_ARCH}.dat"
+  fi
+
+  for fwlist in ${FW_LISTS}; do
+    [ -f ${fwlist} ] || continue
+    while read -r fwline; do
+      [ -z "${fwline}" ] && continue
+      [[ ${fwline} =~ ^#.* ]] && continue
+      [[ ${fwline} =~ ^[[:space:]] ]] && continue
+
+      for fwfile in $(cd ${PKG_BUILD} && eval "find ${fwline}"); do
+        [ -d ${PKG_BUILD}/${fwfile} ] && continue
+        if [ -f ${PKG_BUILD}/${fwfile} ]; then
+          mkdir -p $(dirname ${FW_TARGET_DIR}/brcm/${fwfile})
+            cp -Lv ${PKG_BUILD}/${fwfile} ${FW_TARGET_DIR}/brcm/${fwfile}
+        else
+          echo "ERROR: Firmware file ${fwfile} does not exist - aborting"
+          exit 1
+        fi
+      done
+    done < ${fwlist}
+  done
+}
